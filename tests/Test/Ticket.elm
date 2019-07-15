acbe4643ec097353dbe08e922c1b7ad2033b592d
IzumiSy <beetle-noise@gmx.com>
2019-07-15 20:07:08 +0900

    Preparing for writing some tests

---
+++ tests/Test/Ticket.elm
@@ -0,0 +1,13 @@
+module Test.Ticket exposing (suite)
+
+import DayType
+import Dict
+import Expect
+import Test exposing (..)
+import Ticket
+import Time
+
+
+suite : Test
+suite =
+    todo "write some tests later..."
